71ab1621f97e5f581871f4daca747241f47143b7
kaua <kauavbrandaorocha@gmail.com>
2026-01-30 16:28:49 -0300

    refactor - alteração na forma como os scripts rodam, utilizando função para facilitar a extração

--- extrair_zips.py
+++ extrair_zips.py
@@ -3,23 +3,30 @@ import zipfile
 import os
 import logging
 
-try:
-    # acessa a pasta
-    caminho_zip = "arquivoszip/"
+def extrair_arquivos_zip(caminho_pasta):
+    caminho_zip = caminho_pasta # acessa a pasta
+    arquivos = os.listdir(caminho_zip) # retorna uma lista de todos os arquivos presentes na pasta)
 
-    # retorna uma lista de todos os arquivos presentes na pasta
-    arquivos = os.listdir(caminho_zip)
-
-    # percorre a lista e verifica se algum arquivo tem a terminação .zip para poder unir o caminho da pasta com o nome dos arquivos
+    # percorre a lista e verifica se algum arquivo tem a terminação .zip
     for arquivo in arquivos:
         if arquivo.endswith(".zip"):
-            caminho_completo_arquivo =  os.path.join(caminho_zip, arquivo)
-            
-            # utilizar leitura para descompactar os arquivos .zip
-            with zipfile.ZipFile(caminho_completo_arquivo, 'r') as zip:
-                # debugg: print(f"Extraindo arquivo: {arquivo}")
+            caminho_completo = os.path.join(caminho_zip, arquivo) # une o caminho da pasta com o nome dos arquivos
+        """
+        # todo esse comando serve para criar pastas com o mesmo nome do zip para evitar duplicações em uma situação de grande volume de arquivos por zip
+        nome_zip = os.path.splitext(arquivo)[0] # obtém o nome do arquivo sem a extensão .zip
+        caminho_extraido = os.path.join("arquivos_extraidos/", nome_zip) # cria o caminho para a pasta onde os arquivos serão extraídos
+
+        os.makedirs(caminho_extraido, exist_ok=True) # cria pasta recursivamente e o exist_ok evita erro se ja existir 
+        """
+        try:
+            with zipfile.ZipFile(caminho_completo, 'r') as zip:
+                # debugg
+                print(f"Extraindo arquivo: {arquivo}")
                 zip.extractall("arquivos_extraidos/") # colocar nome qualquer para a pasta que contem os arquivos extraidos
 
-except zipfile.BadZipFile:
-    logging.error(f"Arquivo(s) Zip corrompidos/inválidos.")
+        except zipfile.BadZipFile:
+            logging.error(f"Arquivo Zip corrompido/inválido: {caminho_completo}")
+
+extrair_arquivos_zip("arquivoszip/")
+
 
